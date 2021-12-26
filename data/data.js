4314ad738ead197ecedfc780aa1bbfd9464c7f68
Alalinga <alalinga.mubarik@amalitech.org>
2021-12-26 08:09:28 +0000

    adding project

---
+++ data/data.js
@@ -0,0 +1,14 @@
+
+const envelopes =[
+
+           {id:1,title:'books',budget:20},
+           {id:2,title:'chrismas',budget:50},
+           {id:3,title:'girls',budget:15},
+           {id:4,title:'family',budget:500},
+           {id:5,title:'food',budget:200}
+
+      ];
+
+
+
+export default envelopes;
